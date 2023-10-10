b762c930387bc798371df80397dcd96531462915
leomorabito02 <leonardomorabito02@hotmail.com>
2023-10-10 17:29:32 -0300

    boton atrás arreglado

--- frontend/src/components/BackButton.jsx
+++ frontend/src/components/BackButton.jsx
@@ -1,11 +1,8 @@
 import React from 'react';
-import { useNavigate } from 'react-router-dom';
-import './Components.css'
-
-
+import { useLocation, useNavigate } from 'react-router-dom';
+import './Components.css';
 
 const BackButton = () => {
-
     const navigate = useNavigate();
     const buttonReturn = () => {
         navigate(-1);
@@ -13,10 +10,23 @@ const BackButton = () => {
 
     return (
         <header id="header">
-            <button id="BackButton" onClick={buttonReturn}>Atrás</button>
+            <button id="backButton" onClick={buttonReturn}>Atrás</button>
         </header>
-
     );
 };
 
-export default BackButton;
+function ComponenteE() {
+    const location = useLocation();
+
+    // Lógica para determinar si el botón debe ocultarse en función de la ruta actual
+    const ocultarBoton = location.pathname === '/' || location.pathname === '/home';
+
+    return ocultarBoton ? null : (
+        <div>
+            <BackButton />
+
+        </div>
+    );
+}
+
+export default ComponenteE;
