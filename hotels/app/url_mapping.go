df1e2370caf8fe515123517faa053c8c0c899b0b
caro <caro@pop-os.localdomain>
2023-10-10 18:55:04 -0300

    avances en hotel

---
+++ hotels/app/url_mapping.go
@@ -0,0 +1,23 @@
+package app
+
+import (
+	hotelController "hotels/controllers/hotel"
+
+	log "github.com/sirupsen/logrus"
+)
+
+func mapUrls() {
+	// Hotels Mapping
+	
+	router.GET("/hotels/:HotelId", hotelController.GetHotelById)
+	
+	router.POST("/hotel", hotelController.InsertHotel)
+	// router.POST("/hotels", hotelController.QueueHotels)
+
+	router.PUT("/hotel/:HotelId", hotelController.UpdateHotel)
+	// router.PUT("/hotels", hotelController.QueueHotels)
+
+	router.DELETE("/hotel/:HotelId", hotelController.DeleteHotelById)
+
+	log.Info("Finishing mappings configurations")
+}
