3a206e4a2ad02e80b9a5540cb7628d543b14d052
VICTORIA <2102175@ucc.edu.ar>
2023-10-18 01:17:18 -0300

    correcion de config

--- hotels/config/config.go
+++ hotels/config/config.go
@@ -4,4 +4,8 @@ var (
 	MONGOHOST       = "hotels_db"
 	MONGOPORT       = 27017
 	MONGOCOLLECTION = "hotels"
+
+	MESSAGESHOST     = "messages"
+	MESSAGESPORT     = 9001
+	MESSAGESENDPOINT = "message"
 )
