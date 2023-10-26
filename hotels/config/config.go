1a921d61ae89fda7636c473e7112c72240056d79
VICTORIA <2102175@ucc.edu.ar>
2023-10-26 00:25:09 -0300

    agrego rutas RABBIT

--- hotels/config/config.go
+++ hotels/config/config.go
@@ -5,6 +5,11 @@ var (
 	MONGOPORT       = 27017
 	MONGOCOLLECTION = "hotels"
 
+	RABBITUSER     = "user"
+	RABBITPASSWORD = "password"
+	RABBITHOST     = "rabbit"
+	RABBITPORT     = 5672
+
 	MESSAGESHOST     = "messages"
 	MESSAGESPORT     = 9001
 	MESSAGESENDPOINT = "message"
