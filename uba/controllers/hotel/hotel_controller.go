34bd095f5388ee3b099eaece4a974ec2c6f5fc5c
ignacio-franciosi <2104338@ucc.edu.ar>
2023-10-27 00:04:23 -0300

    user y hotel andan. Booking no :(

--- uba/controllers/hotel/hotel_controller.go
+++ uba/controllers/hotel/hotel_controller.go
@@ -1,11 +1,8 @@
 package hotel
 
 import (
-	"encoding/json"
-	"fmt"
 	"net/http"
 	"strconv"
-	bookingController "uba/controllers/booking"
 	"uba/dto"
 	service "uba/services"
 
@@ -13,45 +10,13 @@ import (
 	log "github.com/sirupsen/logrus"
 )
 
-type CityCodeMapping struct {
-	City string
-	Code string
-}
-
 //Primero hago la funcion para obtener el city code que despues le tengo que pasar al url de amadeus
-
-func GetCityCode() string {
-
-	var insertHotelDto dto.InsertHotelDto
-	cityName := insertHotelDto.City
-
-	// slice de mapeo de City a código
-	mapCityCode := []CityCodeMapping{
-		{City: "Barcelona", Code: "BCN"},
-		{City: "New York", Code: "NYC"},
-		{City: "Paris", Code: "PAR"},
-		{City: "Milan", Code: "MIL"},
-		{City: "Toronto", Code: "YTO"},
-		{City: "Sydney", Code: "SYD"},
-		{City: "Seoul", Code: "SEL"},
-		{City: "Moscow", Code: "MOW"},
-		{City: "London", Code: "LON"},
-	}
-
-	for _, mapp := range mapCityCode {
-		if mapp.City == cityName {
-			return mapp.Code
-		}
-	}
-
-	return ""
-}
 
 // esta funcion se llama cuando desde mongo se hace un post de hotel
 func InsertHotel(c *gin.Context) {
 
-	var insertHotelDto dto.InsertHotelDto
-	err := c.BindJSON(&insertHotelDto)
+	var hotelDto dto.HotelDto
+	err := c.BindJSON(&hotelDto)
 
 	// Error Parsing json param
 	if err != nil {
@@ -60,57 +25,13 @@ func InsertHotel(c *gin.Context) {
 		return
 	}
 
-	apiUrl := "https://test.api.amadeus.com/v1/reference-data/locations/hotels/by-city"
-	apiUrl += "?cityCode=" + GetCityCode()
-	// Crear una request HTTP GET
-	request, err := http.NewRequest("GET", apiUrl, nil)
-	if err != nil {
-		fmt.Println("Error al crear la request:", err)
-		c.JSON(http.StatusInternalServerError, err.Error())
+	hotelDto, er := service.HotelService.InsertHotel(hotelDto)
+	// Error del Insert
+	if er != nil {
+		c.JSON(er.Status(), er)
 		return
 	}
-
-	token := bookingController.GetAmadeustoken()
-	request.Header.Set("Authorization", "Bearer "+token)
-	// Realiza la request HTTP
-	cliente := &http.Client{}
-	respuesta, err := cliente.Do(request)
-	if err != nil {
-		fmt.Println("Error al realizar la request:", err)
-		c.JSON(http.StatusInternalServerError, err.Error())
-		return
-	}
-
-	defer respuesta.Body.Close()
-
-	// Leer la respuesta del amadeus
-	var response struct {
-		Data []struct {
-			HotelID string `json:"hotelId"`
-		} `json:"data"`
-	}
-
-	// Decodificar la respuesta JSON
-	decoder := json.NewDecoder(respuesta.Body)
-	if err := decoder.Decode(&response); err != nil {
-		log.Error("Error al decodificar la respuesta JSON:", err)
-		c.JSON(http.StatusInternalServerError, err.Error())
-		return
-	}
-	/*Este for recorre toda la estructura de Data hasta encontrar HotelId, cuando lo encuentra
-	se lo pasa como parametro a la funcion*/
-	for _, hotel := range response.Data {
-		hotelDto, er := service.HotelService.InsertHotel(insertHotelDto, hotel.HotelID)
-		// Error del Insert
-		if er != nil {
-			c.JSON(er.Status(), er)
-			return
-		}
-		c.JSON(http.StatusCreated, hotelDto)
-		log.Println("ID del hotel:", hotel.HotelID)
-		break
-
-	}
+	c.JSON(http.StatusCreated, hotelDto)
 
 }
 
--- uba/dto/dto_insert_hotel.go
+++
@@ -1,8 +0,0 @@
-package dto
-
-type InsertHotelDto struct {
-	Id      int     `json:"id"`
-	IdMongo string  `json:"id_mongo"`
-	City    string  `json:"city"`
-	Price   float32 `json:"price"`
-}
